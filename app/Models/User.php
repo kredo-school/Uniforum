5da4c1f899a172296eef05c6321dcd87ea43e287
Yukari0810 <nullneo76@gmail.com>
2024-04-26 16:18:26 +0800

    show my team function (in team page)

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function user_team(){
+        return $this->hasMany(UserTeam::class, 'user_id');
+    }
 }
