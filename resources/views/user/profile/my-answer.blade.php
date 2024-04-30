fc26c823422896d53e61cb41b8f48ff584086fe3
Yukari0810 <nullneo76@gmail.com>
2024-04-30 14:43:54 +0800

    show my answers in profile page function

--- resources/views/user/profile/my-answer.blade.php
+++ resources/views/user/profile/my-answer.blade.php
@@ -7,64 +7,55 @@
     <div class="w-85 mx-auto mt-5">
         <div class="row">
             <div class="col text-center">
-                <a href="{{route('profile')}}" class="text-decoration-none dark-purple fs-5">
+                <a href="{{route('profile.view', $detail->id)}}" class="text-decoration-none dark-purple fs-5">
                     my questions
                 </a>
-                {{-- <hr class="menu-hr mt-2"> --}}
+                @if (request()->is('profile/view/*'))
+                <hr class="menu-hr mt-2">
+                @endif
             </div>
             <div class="col text-center">
-                <a href="{{route('profile.myanswer')}}" class="text-decoration-none dark-purple fs-5">
+                <a href="{{route('profile.myanswer', $detail->id)}}" class="text-decoration-none dark-purple fs-5">
                     my answers
                 </a>
+                @if (request()->is('profile/myanswer/*'))
                 <hr class="menu-hr mt-2">
+                @endif
             </div>
             <div class="col text-center">
                 <a href="{{route('profile.myteam')}}" class="text-decoration-none dark-purple fs-5">
                     my teams
                 </a>
-                {{-- <hr class="menu-hr mt-2"> --}}
+                @if (request()->is('profile/myteam/*'))
+                <hr class="menu-hr mt-2">
+                @endif
             </div>
         </div>
         <div class="mt-3">
-            <a href="{{route('view')}}" class="text-decoration-none">
-                <div class="q-content pt-2 px-4 mb-2">
-                    <div class="category-label w-20 ms-auto text-center">
-                        <p class="fs-6">Club</p>
+            @forelse ($my_answers as $my_answer)
+            <a href="{{route("question.show", $my_answer->question->id)}}" class="text-decoration-none">
+                <div class="q-content pt-2 px-4 mb-3">
+                    <div class="category-label w-20 ms-auto text-center mt-1">
+                        <p class="fs-6 m-0">{{$my_answer->question->category->name}}</p>
                     </div>
-                    <div class="text-start">
-                        <h5 class="dark-purple text-start">How can I join the Kredo Soccer Club?</h5>
+                    <div class="text-start my-1">
+                        <h5 class="dark-purple text-start">{{$my_answer->question->title}}</h5>
                     </div>
-                    <div class="text-end">
-                        <form action="">
-                            <button type="submit" class="btn btn-none px-0">
-                                <i class="fa-regular fa-heart purple-gray"></i>
-                            </button>
-                            <span class="purple-gray">1</span>
-                            <span class="ms-3 purple-gray">3/26/2024 15:05</span>
-                        </form>
+                    <div class="text-end pb-1">
+                        <i class="fa-regular fa-heart purple-gray"></i>
+                        <span class="purple-gray">{{$my_answer->question->likes->count()}}</span>
+                        <span class="ms-3 purple-gray">{{$my_answer->question->created_at->format('m/d/Y')}}</span>
                     </div>
                 </div>
             </a>
-
-            <a href="{{route('view')}}" class="text-decoration-none">
-                <div class="q-content pt-2 px-4 mb-2">
-                    <div class="category-label w-20 ms-auto text-center">
-                        <p class="fs-6">Club</p>
-                    </div>
-                    <div class="text-start">
-                        <h5 class="dark-purple text-start">How can I join the Kredo Soccer Club?</h5>
-                    </div>
-                    <div class="text-end">
-                        <form action="">
-                            <button type="submit" class="btn btn-none px-0">
-                                <i class="fa-regular fa-heart purple-gray"></i>
-                            </button>
-                            <span class="purple-gray">1</span>
-                            <span class="ms-3 purple-gray">3/26/2024 15:05</span>
-                        </form>
-                    </div>
-                </div>
-            </a>
+            @empty
+            <div class="pt-5 text-center">
+                <h3 class="mid-gray">No Answers yet</h3>
+            </div>
+            @endforelse
+            <div class="w-100 mt-4">
+                {{ $my_answers->links() }}
+            </div>
 
         </div>
     </div>
