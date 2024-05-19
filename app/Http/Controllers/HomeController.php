56de49e7e4c3795def771615570e53cba58f33ae
Yukari0810 <nullneo76@gmail.com>
2024-05-19 23:20:56 +0800

    additional changes on displaying questions in home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Question;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Category;
+use App\Models\QuestionReport;
 
 class HomeController extends Controller
 {
@@ -32,6 +33,12 @@ class HomeController extends Controller
     {
         $home_questions = $this->question->with('likes')->where('uni_id', Auth::user()->uni_id)->where('team', null)->latest()->paginate(10);
 
+        foreach($home_questions as $key => $home_question){
+            if($home_question->question_report->count() >= QuestionReport::SHADOW_BAN_COUNT){
+                unset($home_questions[$key]);
+            }
+        }
+
         return view('home')->with('home_questions', $home_questions);
     }
 
@@ -43,18 +50,17 @@ class HomeController extends Controller
         }
 
         if($request->search_keyword && $request->search_category){
-            $suggestions = $this->question->where('content', 'LIKE', '%'.$request->search_keyword.'%')->where('category_id', $request->search_category)->where('uni_id', Auth::user()->uni_id)->where('team', null)->get();
+            $suggestions = $this->question->where('content', 'LIKE', '%'.$request->search_keyword.'%')->where('category_id', $request->search_category)->where('uni_id', Auth::user()->uni_id)->where('team', null)->withCount('question_report')->orderBy('question_report_count', 'asc')->get();
             $category_name = $this->category->where('id', $request->search_category)->pluck('name');
         }
         elseif($request->search_keyword){
-            $suggestions = $this->question->where('content', 'LIKE', '%'.$request->search_keyword.'%')->where('uni_id', Auth::user()->uni_id)->where('team', null)->get();
+            $suggestions = $this->question->where('content', 'LIKE', '%'.$request->search_keyword.'%')->where('uni_id', Auth::user()->uni_id)->where('team', null)->withCount('question_report')->orderBy('question_report_count', 'asc')->get();
             $category_name[] = '';
         }
         elseif($request->search_category){
-            $suggestions = $this->question->where('category_id', $request->search_category)->where('uni_id', Auth::user()->uni_id)->get();
+            $suggestions = $this->question->where('category_id', $request->search_category)->where('uni_id', Auth::user()->uni_id)->where('team', null)->withCount('question_report')->orderBy('question_report_count', 'asc')->get();
             $category_name = $this->category->where('id', $request->search_category)->pluck('name');
         }
-
         return view('search-result')->with('suggestions', $suggestions)->with('keyword', $request->search_keyword)->with('category_name', $category_name[0]);
     }
 }
